4e1fec6622916f022eb8d0097e792d4ab2824f3f
Walid shady <walidfear238@gmail.com>
2026-06-07 08:56:33 -0700

    sign in

--- 09-register.php
+++ 09-register.php
@@ -4,7 +4,7 @@ include "db/connect.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'path/to/PHPMailer/src/Exception.php';
+//require 'path/to/PHPMailer/src/Exception.php';
 require 'path/to/PHPMailer/src/PHPMailer.php';
 require 'path/to/PHPMailer/src/SMTP.php';
 
